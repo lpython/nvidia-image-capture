c270f0dfba308fc2c6f1c8f85f243799450a1f50
Alex M <amcnurlan@gmail.com>
2022-10-16 00:22:01 -0700

    compiles atleast

--- source/greeter.cpp
+++ source/greeter.cpp
@@ -1,20 +1,6 @@
-#include <fmt/format.h>
+#include <string>
 #include <greeter/greeter.h>
 
-using namespace greeter;
-
-Greeter::Greeter(std::string _name) : name(std::move(_name)) {}
-
-std::string Greeter::greet(LanguageCode lang) const {
-  switch (lang) {
-    default:
-    case LanguageCode::EN:
-      return fmt::format("Hello, {}!", name);
-    case LanguageCode::DE:
-      return fmt::format("Hallo {}!", name);
-    case LanguageCode::ES:
-      return fmt::format("¡Hola {}!", name);
-    case LanguageCode::FR:
-      return fmt::format("Bonjour {}!", name);
-  }
+std::string non_greeter() {
+  return std::string("wat");
 }
--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -1,53 +1,151 @@
-#include <greeter/greeter.h>
-#include <greeter/version.h>
 
-#include <cxxopts.hpp>
+#include <cstdlib>
+#include <gst/gst.h>
+#include <gst/gstinfo.h>
+#include <gst/app/gstappsrc.h>
+#include <gst/app/gstappsink.h>
+#include <glib-unix.h>
+#include <dlfcn.h>
+
+#include <cstring>
 #include <iostream>
-#include <string>
-#include <unordered_map>
+#include <sstream>
+#include <thread>
 
-auto main(int argc, char** argv) -> int {
-  const std::unordered_map<std::string, greeter::LanguageCode> languages{
-      {"en", greeter::LanguageCode::EN},
-      {"de", greeter::LanguageCode::DE},
-      {"es", greeter::LanguageCode::ES},
-      {"fr", greeter::LanguageCode::FR},
-  };
+using namespace std;
 
-  cxxopts::Options options(*argv, "A program to welcome the world!");
+#define USE(x) ((void)(x))
 
-  std::string language;
-  std::string name;
+static GstPipeline *gst_preview = nullptr;
+static GstPipeline *gst_jpgenc = nullptr;
+static string launch_string;   
+static GstElement *appsrc_;
+static int frame_count = 0;
 
-  // clang-format off
-  options.add_options()
-    ("h,help", "Show help")
-    ("v,version", "Print the current version number")
-    ("n,name", "Name to greet", cxxopts::value(name)->default_value("World"))
-    ("l,lang", "Language code to use", cxxopts::value(language)->default_value("en"))
-  ;
-  // clang-format on
+static void appsink_eos(GstAppSink * appsink, gpointer user_data)
+{
+    printf("app sink receive eos\n");
+//    g_main_loop_quit (hpipe->loop);
+}
 
-  auto result = options.parse(argc, argv);
+static GstFlowReturn new_buffer(GstAppSink *appsink, gpointer user_data)
+{
+    GstSample *sample = NULL;
 
-  if (result["help"].as<bool>()) {
-    std::cout << options.help() << std::endl;
+    frame_count ++;
+    if (frame_count != 123 && frame_count != 168) {
+        return GST_FLOW_OK;
+    }
+
+    g_signal_emit_by_name (appsink, "pull-sample", &sample,NULL);
+
+    if (sample)
+    {
+        GstBuffer *buffer = NULL;
+        GstCaps   *caps   = NULL;
+        GstFlowReturn ret;
+
+        caps = gst_sample_get_caps (sample);
+        if (!caps)
+        {
+            printf("could not get snapshot format\n");
+        }
+        gst_caps_get_structure (caps, 0);
+        buffer = gst_sample_get_buffer (sample);
+
+        gst_buffer_ref(buffer);
+        g_signal_emit_by_name (appsrc_, "push-buffer", buffer, &ret);
+        gst_buffer_unref(buffer);
+        gst_sample_unref (sample);
+    }
+    else
+    {
+        g_print ("could not make snapshot\n");
+    }
+
+    return GST_FLOW_OK;
+}
+
+int main(int argc, char** argv) {
+    USE(argc);
+    USE(argv);
+
+    gst_init (&argc, &argv);
+
+    GMainLoop *main_loop;
+    main_loop = g_main_loop_new (NULL, FALSE);
+    ostringstream launch_stream;
+    int w = 1920;
+    int h = 1080;
+    GstAppSinkCallbacks callbacks = {appsink_eos, NULL, new_buffer};
+
+    launch_stream
+    << "nvcamerasrc ! "
+    << "video/x-raw(memory:NVMM), width="<< w <<", height="<< h <<", framerate=30/1 ! "
+    << "tee name=t1 "
+    << "t1. ! queue ! nvoverlaysink "
+    << "t1. ! queue ! nvvidconv ! "
+    << "video/x-raw, format=I420, width="<< w <<", height="<< h <<" ! "
+    << "appsink name=mysink ";
+
+    launch_string = launch_stream.str();
+
+    g_print("Preview string: %s\n", launch_string.c_str());
+
+    GError *error = nullptr;
+    gst_preview  = (GstPipeline*) gst_parse_launch(launch_string.c_str(), &error);
+
+    if (gst_preview == nullptr) {
+        g_print( "Failed to parse launch: %s\n", error->message);
+        return -1;
+    }
+    if(error) g_error_free(error);
+
+    GstElement *appsink_ = gst_bin_get_by_name(GST_BIN(gst_preview), "mysink");
+    gst_app_sink_set_callbacks (GST_APP_SINK(appsink_), &callbacks, NULL, NULL);
+
+    // jpegenc pipeline
+    {
+        launch_stream.str("");
+        launch_stream.clear();
+        launch_stream
+        << "appsrc name=mysource ! "
+        << "video/x-raw,width="<< w <<",height="<< h <<",format=I420,framerate=1/1 ! "
+        << "nvjpegenc ! "
+        << "multifilesink location=snap-%03d.jpg ";
+
+        launch_string = launch_stream.str();
+
+        g_print("JPEG encoding string: %s\n", launch_string.c_str());
+        gst_jpgenc = (GstPipeline*) gst_parse_launch(launch_string.c_str(), &error);
+
+        if (gst_jpgenc == nullptr) {
+            g_print( "Failed to parse launch: %s\n", error->message);
+            return -1;
+        }
+        if(error) g_error_free(error);
+        appsrc_ = gst_bin_get_by_name(GST_BIN(gst_jpgenc), "mysource");
+        gst_app_src_set_stream_type(GST_APP_SRC(appsrc_), GST_APP_STREAM_TYPE_STREAM);
+    }
+
+    gst_element_set_state((GstElement*)gst_jpgenc, GST_STATE_PLAYING);
+    gst_element_set_state((GstElement*)gst_preview, GST_STATE_PLAYING);
+
+    sleep(10);
+    //g_main_loop_run (main_loop);
+
+    gst_element_set_state((GstElement*)gst_preview, GST_STATE_NULL);
+    gst_object_unref(GST_OBJECT(gst_preview));
+
+    gst_app_src_end_of_stream((GstAppSrc *)appsrc_);
+    // Wait for EOS message
+    GstBus *bus = gst_pipeline_get_bus(GST_PIPELINE(gst_jpgenc));
+    gst_bus_poll(bus, GST_MESSAGE_EOS, GST_CLOCK_TIME_NONE);
+    gst_element_set_state((GstElement*)gst_jpgenc, GST_STATE_NULL);
+    gst_object_unref(GST_OBJECT(gst_jpgenc));
+
+    g_main_loop_unref(main_loop);
+
+    g_print("going to exit \n");
     return 0;
-  }
-
-  if (result["version"].as<bool>()) {
-    std::cout << "Greeter, version " << GREETER_VERSION << std::endl;
-    return 0;
-  }
-
-  auto langIt = languages.find(language);
-  if (langIt == languages.end()) {
-    std::cerr << "unknown language code: " << language << std::endl;
-    return 1;
-  }
-
-  greeter::Greeter greeter(name);
-  std::cout << greeter.greet(langIt->second) << std::endl;
-
-  return 0;
 }
